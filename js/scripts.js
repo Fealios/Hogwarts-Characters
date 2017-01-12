ecf0c27d86610bf617b823ef11659489cc605cbf
melvin gruschow and sandro mijatovic <melvin@>
2017-01-12 14:26:13 -0800

    Pictures wont appear

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,27 @@ $(document).ready(function(){
     choice3 = parseInt($('#topic').val());
     choice4 = parseInt($('#who').val());
     total = choice1 + choice2 + choice3 + choice4;
-    // console.log(total);
+    // console.log('complete math');
+    $("#output").show();
+    $('.pic').hide();
 
     if (total <= 3) {
       $("#output").text("Harry");
+      $("#harry").show();
+      console.log(total);
     } else if (total > 3 || total <= 6) {
       $("#output").text("Ron");
+      $("#ron").show();
+      console.log(total)
     } else if (total > 6 || total <= 9) {
        $("#output").text("Hermione");
+       $("#hermione").show();
     } else if (total > 9 || total <= 12) {
        $("#output").text("Draco");
+       $("#draco").show();
     }
+
+
 
     event.preventDefault();
   });
